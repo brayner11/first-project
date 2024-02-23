fbc875780a8ff7137b08e7ff257d0f819527a431
LeonTGC <leon.cooper@gceducationandskills.ac.uk>
2024-02-23 12:18:29 +0000

    added onClick

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { useState } from 'react'
+import Form from './Components/Form'
 
 const App = () => {
   const [persons, setPersons] = useState([])
   const [nameInput, setNameInput] = useState("")
   const [ageInput, setAgeInput] = useState("")
+
+
   return (
     <div>
       <h1>Introduction to React</h1>
+      <Form />
     </div>
   )
 }
--- src/Components/Form.js
+++ src/Components/Form.js
@@ -3,17 +3,23 @@ import { useState } from "react"
 const Form = () => {
     const [input, setInput] = useState('')
 
-    const handleSubmit = () => {}
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        alert(`${input} says hello!!!`)
+    }
 
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <label>
                 Enter your name:
                 <input 
-                
-
+                    type="text"
+                    value={input}
+                    onChange={(event) => setInput(event.target.value)}
                 />
             </label>
+            <h1>{input}</h1>
+            <input type="submit" />
         </form>
     )
 }
